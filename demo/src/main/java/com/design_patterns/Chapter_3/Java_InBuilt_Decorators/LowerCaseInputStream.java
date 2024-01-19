65657ddcd624ba8f7b4f6e544d628fd8be193dc3
Anant Agarwal <anantagarwal@Anants-MacBook-Air.local>
2024-01-19 20:30:26 +0530

    added the io section for chapter_3

--- demo/src/main/java/com/design_patterns/Chapter_3/Java_InBuilt_Decorators/LowerCaseInputStream.java
+++ demo/src/main/java/com/design_patterns/Chapter_3/Java_InBuilt_Decorators/LowerCaseInputStream.java
@@ -18,8 +18,8 @@ public class LowerCaseInputStream extends FilterInputStream {
     public int read(byte[] b, int offset, int len) throws IOException{
         int result = super.read(b, offset, len);
         for(int i = offset; i < offset + result; i++){
-
+            b[i] = (byte)Character.toLowerCase((char)b[i]);
         }
-        return -1;
+        return result;
     }
 }
